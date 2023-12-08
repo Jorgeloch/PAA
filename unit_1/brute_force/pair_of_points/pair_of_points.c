f068395727cc29f6d275c1cd217362efa9f35175
Jorge Loch <jorgembloch2001@gmail.com>
2023-12-08 14:01:18 -0300

    refactoring pair of points program

--- unit_1/brute_force/pair_of_points/pair_of_points.c
+++ unit_1/brute_force/pair_of_points/pair_of_points.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <math.h>
 
 typedef struct coordinates {
@@ -16,20 +17,21 @@ int main (void)
   {
     scanf("%i %i", &(points[i].x), &(points[i].y)); 
   }
-  double min_distance = 1500;
+  long int min_distance = LONG_MAX;
   for (int i = 0; i < n - 1; i++)
   {
     for (int j = i + 1; j < n; j++)
     {
-      unsigned int dx = abs(points[i].x - points[j].x);
-      unsigned int dy = abs(points[i].y - points[j].y);
-      double distance = sqrt((double) ((dx * dx) + (dy * dy)));
+      int dx = points[i].x - points[j].x;
+      int dy = points[i].y - points[j].y;
+      long int distance = (dx * dx) + (dy * dy);
       if (distance < min_distance)
       {
         min_distance = distance;
       }
     }
   }
-  printf("%.4lf\n", min_distance);
+  double result = sqrt((double) min_distance);
+  printf("%.4lf\n", result);
   free(points);
 }
